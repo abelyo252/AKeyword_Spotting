f0810498b4135555843e06a60f01f15971973969
AdemSeid <119794583+AdemSeid@users.noreply.github.com>
2023-05-12 23:48:38 +0300

    Update key_spotter.py

--- key_spotter.py
+++ key_spotter.py
@@ -9,6 +9,7 @@ import argparse
 import os
 import time
 import wave
+from itertools import zip_longest
 
 # Import non-standard modules.
 import numpy as np
@@ -24,10 +25,11 @@ import tqdm
 
 
 class Keyword_Spotter:
-    def __init__(self, model_path, audio_length, audio_path):
+    def __init__(self, model_path, audio_length, audio_path,dec_conf):
         self.model_path = model_path
         self.audio_length = audio_length
         self.audio_path = audio_path
+        self.dec_conf = dec_conf
 
         self.RATE = 16000
         self.CHUNK_SIZE = self.audio_length
@@ -66,13 +68,28 @@ class Keyword_Spotter:
         return audio, sr
 
     def get_model(self):
-        sess = onnxruntime.InferenceSession(self.model_path)
-        return sess
+        try:
+            sess = onnxruntime.InferenceSession(self.model_path)
+            return sess
+        except Exception as e:
+            return "[-] Couldn't get model or Model is Invalid: {}".format(e)
 
     def syntax_helper(self):
-        print("Syntax: python key_spotter.py --al <audio_length> --model <model_path> --a <audio_path>")
+        print("Syntax: python key_spotter.py --al <audio_length> --model <model_path> --a <audio_path> --conf <detection_confidence>")
 
     def file_checker(self):
+
+        if self.audio_length != 48000:
+            print("\033[91mInvalid Audio Length Passed , Model prepared for analysed 48000 sample!\033[0m")
+            self.syntax_helper()
+            exit()
+
+        if not 0 <= self.dec_conf <= 1:
+            print("\033[91mInvalid Detection Confidence passed!\033[0m")
+            self.syntax_helper()
+            exit()
+
+            
         if not os.path.isfile(self.model_path):
             print("\033[91mInvalid model path or file not found!\033[0m")
             self.syntax_helper()
@@ -83,6 +100,7 @@ class Keyword_Spotter:
             self.syntax_helper()
             exit()
 
+        
     def audio_chunker(self):
 
         # divide the audio into chunks of 48000 samples
@@ -128,7 +146,7 @@ class Keyword_Spotter:
         try:
             y_probs = self.model.run(None, input_data)
             arr = y_probs[0]
-            selected_values = arr[arr > 0.5]
+            selected_values = arr[arr > self.dec_conf]
 
             # Convert the output to class predictions
             if len(selected_values) == 0:
@@ -167,23 +185,17 @@ class Keyword_Spotter:
         print()
         time1 = 0
         time2 = 2
-        for disp in zip(self.result1,self.result2):
-            if disp != None:
+        for disp in zip_longest(self.result1,self.result2):
+            
                 minute1 = int(time1 / 60)
                 second1 = time1 % 60
                 minute2 = int(time2 / 60)
                 second2 = time2 % 60
                 print(f"onDetect Seq: {disp[0]} -> around {minute1:02d}:{second1:02d},  Intermediatary: {disp[1]} -> around {minute2:02d}:{second2:02d}")
-            time1 = time1 + 3
-            time2 = time2 + 3
+                time1 = time1 + 3
+                time2 = time2 + 3
 
-##        time = 2
-##        for disp in self.result2:
-##            if disp != None:
-##                minute = int(time / 60)
-##                second = time % 60
-##                print(f'onDetect Intermediatary:  {disp} -> around {minute}:{second}')
-##            time = time + 3
+
         print(f"")
         print(
             """{}[*] Model Inference time for the given audio: {} second [*]{}\n""".format(
@@ -196,9 +208,10 @@ def main():
     parser.add_argument("--al", type=int, help="audio length")
     parser.add_argument("--model", type=str, help="model path")
     parser.add_argument("--a", type=str, help="audio path")
+    parser.add_argument("--conf", type=float, help="Detection Confidence")
     args = parser.parse_args()
 
-    ks = Keyword_Spotter(args.model, args.al, args.a)
+    ks = Keyword_Spotter(args.model, args.al, args.a , args.conf)
     ks.file_checker()
     ks.audio_chunker()
     ptime = time.time()
